b0a5acfb1a12830025b93cf1ceef70f6f761b38b
zwgmy <36669321+zwgmy@users.noreply.github.com>
2024-11-03 11:47:01 +0800

    Update wf183de_sensor.h

--- components/wf183de/wf183de_sensor.h
+++ components/wf183de/wf183de_sensor.h
@@ -16,7 +16,8 @@ class WF183DESensor : public PollingComponent {
   Sensor *get_temperature_sensor() { return temperature_sensor; }
   Sensor *get_pressure_sensor() { return pressure_sensor; }
 
-  void setup() override {
+  void setup() override 
+  {
     ESP_LOGD("WF183DE", "Sensor setup completed");
   }
 
@@ -25,7 +26,8 @@ class WF183DESensor : public PollingComponent {
     Wire.beginTransmission(WF183DE_I2C_ADDRESS);  // I2C 地址
     Wire.write(0x0A);  // 温度命令寄存器
     Wire.write(0x04);  // 触发温度转换命令
-    if (Wire.endTransmission() != 0) {
+    if (Wire.endTransmission() != 0) 
+    {
       ESP_LOGE("WF183DE", "Failed to start temperature conversion");
       return;
     }
@@ -37,12 +39,14 @@ class WF183DESensor : public PollingComponent {
     Wire.endTransmission();
     //delay(10);  // 再次延迟以确保数据可用
     Wire.requestFrom(WF183DE_I2C_ADDRESS, 2);
-    if (Wire.available() == 2) {
+    if (Wire.available() == 2) 
+    {
       int16_t temp_raw = (Wire.read() << 8) | Wire.read();
       float temperature = temp_raw / 10.0;
       ESP_LOGD("WF183DE", "Temperature raw: %d, converted: %.1f", temp_raw, temperature);
       temperature_sensor->publish_state(temperature);
-    } else {
+    } else 
+    {
       ESP_LOGE("WF183DE", "Failed to read temperature data");
     }
 
@@ -50,7 +54,8 @@ class WF183DESensor : public PollingComponent {
     Wire.beginTransmission(WF183DE_I2C_ADDRESS);
     Wire.write(0x0A);
     Wire.write(0x06);  // 压力命令
-    if (Wire.endTransmission() != 0) {
+    if (Wire.endTransmission() != 0) 
+    {
       ESP_LOGE("WF183DE", "Failed to start pressure conversion");
       return;
     }
@@ -62,15 +67,16 @@ class WF183DESensor : public PollingComponent {
     Wire.endTransmission();
     //delay(10);  // 再次延迟以确保数据可用
     Wire.requestFrom(WF183DE_I2C_ADDRESS, 4);
-    if (Wire.available() == 4) {
+    if (Wire.available() == 4) 
+    {
       uint32_t pressure_raw = (Wire.read() << 24) | (Wire.read() << 16) | (Wire.read() << 8) | Wire.read();
       float pressure = pressure_raw / 1000.0;
       ESP_LOGD("WF183DE", "Pressure raw: %u, converted: %.1f", pressure_raw, pressure);
       pressure_sensor->publish_state(pressure);
     } else {
       ESP_LOGE("WF183DE", "Failed to read pressure data");
-    }
-  }
+           }
+  }  //void update() override
 };
 
 }  // namespace wf183de
