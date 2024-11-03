e51ac7a2a1610cb21fd9d46d442125e00d0de3ff
zwgmy <36669321+zwgmy@users.noreply.github.com>
2024-11-03 15:22:24 +0800

    Update __init__.py

--- components/wf183de/__init__.py
+++ components/wf183de/__init__.py
@@ -1,20 +1,20 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c
-from ..bmp280_base import to_code_base, CONFIG_SCHEMA_BASE
+#from ..bmp280_base import to_code_base, CONFIG_SCHEMA_BASE
 
-AUTO_LOAD = ["bmp280_base"]
-CODEOWNERS = ["@ademuri"]
+#AUTO_LOAD = ["bmp280_base"]
+CODEOWNERS = ["@zwgmy"]
 DEPENDENCIES = ["i2c"]
 
-bmp280_ns = cg.esphome_ns.namespace("bmp280_i2c")
-BMP280I2CComponent = bmp280_ns.class_(
-    "BMP280I2CComponent", cg.PollingComponent, i2c.I2CDevice
+wf183de_ns = cg.esphome_ns.namespace("wf183de")
+WF183DEComponent = wf183de_ns.class_(
+    "WF183DEComponent", cg.PollingComponent, i2c.I2CDevice
 )
 
 CONFIG_SCHEMA = CONFIG_SCHEMA_BASE.extend(
-    i2c.i2c_device_schema(default_address=0x77)
-).extend({cv.GenerateID(): cv.declare_id(BMP280I2CComponent)})
+    i2c.i2c_device_schema(default_address=0x6D)
+).extend({cv.GenerateID(): cv.declare_id(WF183DEComponent)})
 
 
 async def to_code(config):
