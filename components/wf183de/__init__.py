5ef3c20904479fb0a6405d1c18d75944e15686b5
zwgmy <36669321+zwgmy@users.noreply.github.com>
2024-11-03 10:05:44 +0800

    Update __init__.py

--- components/wf183de/__init__.py
+++ components/wf183de/__init__.py
@@ -1,9 +1,1 @@
-import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome import automation
-from esphome.components import i2c
-from esphome.const import CONF_ID, CONF_SIZE
-
-DEPENDENCIES = ["i2c"]
-MULTI_CONF = True
-
+#################
