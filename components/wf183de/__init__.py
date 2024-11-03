c3a3734aa6d6a3d4955347b0c35dcbd3a6f4bc50
zwgmy <36669321+zwgmy@users.noreply.github.com>
2024-11-03 09:50:17 +0800

    Update __init__.py

--- components/wf183de/__init__.py
+++ components/wf183de/__init__.py
@@ -1,1 +1,9 @@
 # Required for ESPHome external component recognition
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome import automation
+from esphome.components import i2c
+from esphome.const import CONF_ID, CONF_SIZE
+
+DEPENDENCIES = ["i2c"]
+MULTI_CONF = True
